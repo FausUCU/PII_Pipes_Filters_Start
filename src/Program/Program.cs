586bbbab94863cc1173386e6dd89a1467e222f70
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-10-18 20:52:56 -0300

    Ejercicio 1 completo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,15 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider= new PictureProvider();
+            IPicture picture= provider.GetPicture("C:/Users/faust/OneDrive/Escritorio/Programacion02/Ejercisios/Imagenes/mrKrab.jpg");
+            PipeNull pipeNull=new PipeNull();
+            FilterNegative filterNegative= new FilterNegative();
+            PipeSerial pipeSerial02=new PipeSerial(filterNegative,pipeNull);
+            FilterGreyscale filterGreyscale=new FilterGreyscale();
+            PipeSerial pipeSerial01=new PipeSerial(filterGreyscale,pipeSerial02);
+            pipeSerial01.Send(picture);
+            provider.SavePicture(picture,"C:/Users/faust/OneDrive/Escritorio/Programacion02/Ejercisios/Imagenes/save/filtro02.jpg");
         }
     }
 }
