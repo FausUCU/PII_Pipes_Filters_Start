abc9f05af51ce526f2ba1bf66cdf3ca5cbfa3333
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-10-20 10:45:50 -0300

    guardado de imagen filtrada

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,21 +2,28 @@
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
 
+
 namespace CompAndDel
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //cre una carpeta llamada "filtradas" dentro de program para guardar las fotos que voy aciendo//
             PictureProvider provider= new PictureProvider();
-            IPicture picture= provider.GetPicture("C:/Users/faust/OneDrive/Escritorio/Programacion02/Ejercisios/Imagenes/mrKrab.jpg");
+            IPicture picture= provider.GetPicture("luke.jpg");
             PipeNull pipeNull=new PipeNull();
             FilterNegative filterNegative= new FilterNegative();
             PipeSerial pipeSerial02=new PipeSerial(filterNegative,pipeNull);
             FilterGreyscale filterGreyscale=new FilterGreyscale();
             PipeSerial pipeSerial01=new PipeSerial(filterGreyscale,pipeSerial02);
-            pipeSerial01.Send(picture);
-            provider.SavePicture(picture,"C:/Users/faust/OneDrive/Escritorio/Programacion02/Ejercisios/Imagenes/save/filtro02.jpg");
+            IPicture pic=pipeSerial01.Send(picture); //Tengo que convertir la funcion de pipeserial.send en su propia instancia de Ipicture, esa instancia conserv a los cambios//
+            provider.SavePicture(pic,"filtradas/luke02.jpg"); //esto guarda la seginda instancia de IPicture (con los filtros aplicados) en la direccion desada//
+            
+            
+           
+
+             
         }
     }
 }
